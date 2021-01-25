54cec9925ffc3ed23bc3c61dfa9b5b75b0a108a5
etezare <jessetezare@gmail.com>
2021-01-24 21:43:26 -0600

    search added

--- Project-Book-Rental/public/angular-app/book-list/book-list-controller.js
+++ Project-Book-Rental/public/angular-app/book-list/book-list-controller.js
@@ -1,15 +1,22 @@
-angular.module("bookRental").controller("booksController",booksController);
-function booksController(bookDataFactory){
-    var vm=this;
-    vm.home="Book Rental Online Home Page";
-    vm.isSubmitted=false;
-    bookDataFactory.getAllBooks().then(function(response){
-        vm.books=response;
-    })
-    vm.addBook=function(){
-      window.location = "#!/addbook";
-  }
-    
-    
-    }
-    
+angular.module("bookRental").controller("booksController", booksController);
+function booksController(bookDataFactory) {
+  var vm = this;
+  vm.home = "Book Rental Online Home Page";
+  vm.isSubmitted = false;
+  bookDataFactory.getAllBooks().then(function (response) {
+    vm.books = response;
+  });
+  
+  //   vm.addBook=function(){
+  //     window.location = "#!/addbook";
+  // }
+  // vm.search = function ($event) {
+  //   $event.preventDefault()
+  //   var searchBook = vm.searchBook;
+  //   let books = vm.books;
+  //   if ((vm.searchBook = "")) {
+  //     vm.books = books;
+  //   }
+  //   vm.books = vm.books.filter((book) => book.title == searchBook);
+  // };
+}
